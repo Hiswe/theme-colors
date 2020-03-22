0065e8022067d0414b2b8209bf4fb71d526acb72
Hiswe <hiswehalya@gmail.com>
2020-03-22 19:44:02 +0100

    chore: add default config

--- node.config.js
+++ node.config.js
@@ -4,18 +4,18 @@ const config = rc(`tc`, {
   PORT: 3666,
   HOST: `localhost`,
   colors: {
-    // 'dark-blue': `#2475a5`,
-    // 'pink': `#e0807c`,
-    // 'yellow': `#f29100`,
-    // 'sky-blue': `#0fb0e5`,
-    // primary: `#62a7a5`, // sky blue darkened (better for contrast)
-    // secondary: `#18223e`, // deep blue
-    // accent: `#87cbc9`, // sky blue
+    'dark-blue': `#2475a5`,
+    'pink': `#e0807c`,
+    'yellow': `#f29100`,
+    'sky-blue': `#0fb0e5`,
+    'primary': `#62a7a5`, // sky blue darkened (better for contrast)
+    'secondary': `#18223e`, // deep blue
+    'accent': `#87cbc9`, // sky blue
     // keep the same as default Vuetify
-    // error: `#ff5252`,
-    // info: `#2196f3`,
-    // success: `#4caf50`,
-    // warning: `#fb8c00`,
+    'error': `#ff5252`,
+    'info': `#2196f3`,
+    'success': `#4caf50`,
+    'warning': `#fb8c00`,
   },
 })
 
